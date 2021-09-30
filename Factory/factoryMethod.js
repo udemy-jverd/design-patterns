1f8ecc5a41f28b231f8e4541cba31c0bf50ede58
joffreyverd <jverd@protonmail.com>
2021-09-30 08:58:33 +0200

    feature(Factory): add factory method example

---
+++ Factory/factoryMethod.js
@@ -0,0 +1,34 @@
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static newCartesianPoint(x, y) {
+        return new Point(x, y);
+    }
+
+    static newPolarPoint(rho, theta) {
+        return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
+    }
+
+    static get factory() {
+        return new PointFactory();
+    }
+}
+
+class PointFactory {
+    static newCartesianPoint(x, y) {
+        return new Point(x, y);
+    }
+
+    static newPolarPoint(rho, theta) {
+        return new Point(rho * Math.cos(theta), rho * Math.sin(theta));
+    }
+}
+
+let p1 = PointFactory.newPolarPoint(5, Math.PI / 2);
+console.log(p1);
+
+let p2 = PointFactory.newCartesianPoint(2, 3);
+console.log(p2);
